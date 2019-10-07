bd7f4b189ab1013147260cf8c7f6ed020bafb163
伍双 <beingswu@gmail.com>
2019-10-07 18:35:29 +0800

    fix bug

--- src/pages/RoomContext.ts
+++ src/pages/RoomContext.ts
@@ -1,16 +1,9 @@
 import * as React from "react";
 import {Room} from "white-react-sdk";
 export type RoomContext = {
-    // readonly remoteMediaStreams: any[];
-    // readonly userId: number;
-    // readonly localStream: any;
-    // setSliderFloating: () => void;
-    // setSliderExtending: () => void;
-    // setSliderHiding: () => void;
     whiteboardLayerDownRef: HTMLDivElement;
     room: Room;
     onColorArrayChange?: (colorArray: string[]) => void;
-    // joinRoomTime: number;
 };
 
 const context = React.createContext<RoomContext>(undefined as any);
