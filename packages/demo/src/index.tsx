d820e9331e1f1a23d0e5a6852355f79f7556c910
buhe <hare.bu@herewhite.com>
2019-10-09 12:00:45 +0800

    move to monorepo

--- packages/demo/src/index.tsx
+++ packages/demo/src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {AppRoutes} from "./pages/AppRoutes";
+import WhiteFastSDK from "white-fast-web-sdk";
+console.log(WhiteFastSDK.Room);
 ReactDOM.render(
     <AppRoutes/>,
     document.getElementById("app-root"),
