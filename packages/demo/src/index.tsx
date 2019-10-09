aaa0eab977bc1aadb565dd20f0817c67300deb94
buhe <hare.bu@herewhite.com>
2019-10-09 12:07:59 +0800

    auto reload

--- packages/demo/src/index.tsx
+++ packages/demo/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {AppRoutes} from "./pages/AppRoutes";
 import WhiteFastSDK from "white-fast-web-sdk";
-console.log(WhiteFastSDK.Room);
+console.log(WhiteFastSDK);
 ReactDOM.render(
     <AppRoutes/>,
     document.getElementById("app-root"),
--- packages/white-fast-web-sdk/src/facade/index.ts
+++ packages/white-fast-web-sdk/src/facade/index.ts
@@ -1,8 +1,9 @@
 import { RoomFacade, PlayerFacade } from "./Facade";
-type FastSDK = { Room: any, Player: any };
+type FastSDK = { Room: any, Player: any , Version: string};
 const WhiteFastSDK: FastSDK = {
     Room: RoomFacade,
     Player: PlayerFacade,
+    Version: "1.0.3",
 };
 (window as any).WhiteFastSDK = WhiteFastSDK;
 export default WhiteFastSDK;
