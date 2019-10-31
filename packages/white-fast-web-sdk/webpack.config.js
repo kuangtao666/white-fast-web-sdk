4abcfb171ca654c1edb56a04ccc188bf3f78a7f5
伍双 <beingswu@gmail.com>
2019-10-31 14:10:31 +0800

    fix bug

--- packages/white-fast-web-sdk/webpack.config.js
+++ packages/white-fast-web-sdk/webpack.config.js
@@ -59,7 +59,6 @@ module.exports = {
                     {
                         loader: 'less-loader', // compiles Less to CSS
                         options: {
-                            modifyVars: require("./theme").antd,
                             javascriptEnabled: true,
                         }
                     },
